eeee1739cb23f82348276ffb1119327c8c0cdd02
Sendil Kumar <sendilkumarn@live.com>
2018-02-19 22:43:46 +0100

    Added account resource

---
+++ generators/server/templates/src/main/java/package/web/rest/_AccountResource.kt
@@ -0,0 +1,355 @@
+<%#
+Copyright 2013-2018 the original author or authors from the JHipster project.
+
+This file is part of the JHipster project, see http://www.jhipster.tech/
+for more information.
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-%>
+package <%=packageName%>.web.rest
+
+<%_ if (authenticationType === 'oauth2') { _%>
+    <%_ if (applicationType === 'monolith') { _%>
+import <%=packageName%>.service.UserService
+import <%=packageName%>.service.dto.UserDTO
+    <%_ } else { _%>
+import <%=packageName%>.domain.User
+    <%_ } _%>
+import <%=packageName%>.web.rest.errors.InternalServerErrorException
+
+import com.codahale.metrics.annotation.Timed
+import org.slf4j.LoggerFactory
+    <%_ if (applicationType !== 'monolith') { _%>
+import org.springframework.security.core.GrantedAuthority
+    <%_ } _%>
+import org.springframework.security.oauth2.provider.OAuth2Authentication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import javax.servlet.http.HttpServletRequest
+import java.security.Principal
+    <%_ if (applicationType !== 'monolith') { _%>
+import java.util.stream.Collectors
+    <%_ } _%>
+
+/**
+ * REST controller for managing the current user's account.
+ */
+@RestController
+@RequestMapping("/api")
+class AccountResource<% if (applicationType === 'monolith') { %>(private val userService: UserService)<% } %> {
+
+    private val log = LoggerFactory.getLogger(AccountResource::class.java)
+    
+
+    /**
+     * GET  /authenticate : check if the user is authenticated, and return its login.
+     *
+     * @param request the HTTP request
+     * @return the login if the user is authenticated
+     */
+    @GetMapping("/authenticate")
+    @Timed
+    fun isAuthenticated(request: HttpServletRequest): String {
+        log.debug("REST request to check if the current user is authenticated")
+        return request.remoteUser
+    }
+
+    /**
+     * GET  /account : get the current user.
+     *
+     * @param principal the current user; resolves to null if not authenticated
+     * @return the current user
+     * @throws InternalServerErrorException 500 (Internal Server Error) if the user couldn't be returned
+     */
+    @GetMapping("/account")
+    @Timed
+    @SuppressWarnings("unchecked")
+    <%_ if (applicationType === 'monolith') { _%>
+    fun getAccount(principal: Principal?): UserDTO? {
+        if (principal != null) {
+            if (principal is OAuth2Authentication) {
+                return userService.getUserFromAuthentication(principal as OAuth2Authentication?)
+            } else {
+                // Allow Spring Security Test to be used to mock users in the database
+                return userService.userWithAuthorities
+                    .map { UserDTO(it) }
+                    .orElseThrow { InternalServerErrorException("User could not be found") }
+            }
+        } else {
+            throw InternalServerErrorException("User could not be found");
+        }
+    }
+    <%_ } else { _%>
+    fun getAccount(principal: Principal): User? {
+        return Optional.ofNullable(principal)
+            .filter { it -> it is OAuth2Authentication }
+            .map<Authentication> { it -> (it as OAuth2Authentication).userAuthentication }
+            .map { authentication ->
+                val details = authentication.details as Map<*, *>
+                var activated: Boolean? = false
+                if (details["email_verified"] != null) {
+                    activated = details["email_verified"] as Boolean
+                }
+                User(
+                    authentication.name,
+                    details["given_name"] as String,
+                    details["family_name"] as String,
+                    details["email"] as String,
+                    details["langKey"] as String,
+                    details["picture"] as String,
+                    activated!!,
+                    authentication.authorities.
+                        map { it.authority }
+                        .toSet()
+                )
+            }
+            .orElseThrow { InternalServerErrorException("User could not be found") }
+    }
+    <%_ } _%>
+}
+<%_ } else { _%>
+import com.codahale.metrics.annotation.Timed
+
+<%_ if (authenticationType === 'session') { _%>
+import <%=packageName%>.domain.PersistentToken
+import <%=packageName%>.repository.PersistentTokenRepository
+<%_ } _%>
+import <%=packageName%>.domain.User
+import <%=packageName%>.repository.UserRepository
+import <%=packageName%>.security.SecurityUtils
+import <%=packageName%>.service.MailService
+import <%=packageName%>.service.UserService
+import <%=packageName%>.service.dto.UserDTO
+import <%=packageName%>.web.rest.errors.*
+import <%=packageName%>.web.rest.vm.KeyAndPasswordVM
+import <%=packageName%>.web.rest.vm.ManagedUserVM
+
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+import javax.servlet.http.HttpServletRequest
+import javax.validation.Valid
+import <%=packageName%>.service.dto.PasswordChangeDTO
+<%_ if (authenticationType === 'session') { _%>
+import java.io.UnsupportedEncodingException
+import java.net.URLDecoder
+<%_ } _%>
+import java.util.*
+
+/**
+ * REST controller for managing the current user's account.
+ */
+@RestController
+@RequestMapping("/api")
+class AccountResource(private val userRepository: UserRepository,
+    private val userService: UserService, 
+    private val mailService: MailService<% if (authenticationType === 'session') { %>, private val persistentTokenRepository: PersistentTokenRepository <% } %>) {
+
+    private val log = LoggerFactory.getLogger(AccountResource::class.java)
+
+    /**
+     * POST  /register : register the user.
+     *
+     * @param managedUserVM the managed user View Model
+     * @throws InvalidPasswordException 400 (Bad Request) if the password is incorrect
+     * @throws EmailAlreadyUsedException 400 (Bad Request) if the email is already used
+     * @throws LoginAlreadyUsedException 400 (Bad Request) if the login is already used
+     */
+    @PostMapping("/register")
+    @Timed
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerAccount(@Valid @RequestBody managedUserVM: ManagedUserVM) {
+        if (!checkPasswordLength(managedUserVM.password!!)) {
+            throw InvalidPasswordException()
+        }
+        userRepository.findOneByLogin(managedUserVM.login.toLowerCase()).ifPresent { _ -> throw LoginAlreadyUsedException() }
+        userRepository.findOneByEmailIgnoreCase(managedUserVM.email).ifPresent { _ -> throw EmailAlreadyUsedException() }
+        val user = userService.registerUser(managedUserVM, managedUserVM.password!!)
+        mailService.sendActivationEmail(user)
+    }
+
+    /**
+     * GET  /activate : activate the registered user.
+     *
+     * @param key the activation key
+     * @throws RuntimeException 500 (Internal Server Error) if the user couldn't be activated
+     */
+    @GetMapping("/activate")
+    @Timed
+    fun activateAccount(@RequestParam(value = "key") key: String) {
+        val user = userService.activateRegistration(key)
+        if (!user.isPresent) {
+            throw InternalServerErrorException("No user was found for this reset key")
+        }
+    }
+
+    /**
+     * GET  /authenticate : check if the user is authenticated, and return its login.
+     *
+     * @param request the HTTP request
+     * @return the login if the user is authenticated
+     */
+    @GetMapping("/authenticate")
+    @Timed
+    fun isAuthenticated(request: HttpServletRequest): String {
+        log.debug("REST request to check if the current user is authenticated")
+        return request.remoteUser
+    }
+
+    /**
+     * GET  /account : get the current user.
+     *
+     * @return the current user
+     * @throws RuntimeException 500 (Internal Server Error) if the user couldn't be returned
+     */
+    @GetMapping("/account")
+    @Timed
+    fun getAccount(): UserDTO {
+        return userService.getUserWithAuthorities()
+            .map { UserDTO(it) }
+            .orElseThrow { InternalServerErrorException("User could not be found") }
+    }
+
+    /**
+     * POST  /account : update the current user information.
+     *
+     * @param userDTO the current user information
+     * @throws EmailAlreadyUsedException 400 (Bad Request) if the email is already used
+     * @throws RuntimeException 500 (Internal Server Error) if the user login wasn't found
+     */
+    @PostMapping("/account")
+    @Timed
+    fun saveAccount(@Valid @RequestBody userDTO: UserDTO) {
+        val userLogin = SecurityUtils.getCurrentUserLogin().orElseThrow { InternalServerErrorException("Current user login not found") }
+        val existingUser = userRepository.findOneByEmailIgnoreCase(userDTO.email)
+        if (existingUser.isPresent && !existingUser.get().login.equals(userLogin, ignoreCase = true)) {
+            throw EmailAlreadyUsedException()
+        }
+        val user = userRepository.findOneByLogin(userLogin)
+        if (!user.isPresent) {
+            throw InternalServerErrorException("User could not be found")
+        }
+        userService.updateUser(userDTO.firstName, userDTO.lastName, userDTO.email,
+             userDTO.langKey<% if (databaseType === 'mongodb' || databaseType === 'couchbase' || databaseType === 'sql') { %>, userDTO.imageUrl<% } %>)
+     }
+
+    /**
+     * POST  /account/change-password : changes the current user's password
+     *
+     * @param passwordChangeDto current and new password
+     * @throws InvalidPasswordException 400 (Bad Request) if the new password is incorrect
+     */
+    @PostMapping(path = arrayOf("/account/change-password"))
+    @Timed
+     fun changePassword(@RequestBody passwordChangeDto: PasswordChangeDTO) {
+        if (!checkPasswordLength(passwordChangeDto.newPassword)) {
+            throw InvalidPasswordException()
+        }
+        userService.changePassword(passwordChangeDto.currentPassword, passwordChangeDto.newPassword)
+    }<% if (authenticationType === 'session') { %>
+
+    /**
+     * GET  /account/sessions : get the current open sessions.
+     *
+     * @return the current open sessions
+     * @throws RuntimeException 500 (Internal Server Error) if the current open sessions couldn't be retrieved
+     */
+    @GetMapping("/account/sessions")
+    @Timed
+    fun getCurrentSessions(): List<PersistentToken> {
+        return persistentTokenRepository.findByUser(
+            userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin()
+                .orElseThrow { InternalServerErrorException("Current user login not found") })
+                .orElseThrow { InternalServerErrorException("User could not be found") }
+        )
+    }
+
+    /**
+     * DELETE  /account/sessions?series={series} : invalidate an existing session.
+     *
+     * - You can only delete your own sessions, not any other user's session
+     * - If you delete one of your existing sessions, and that you are currently logged in on that session, you will
+     *   still be able to use that session, until you quit your browser: it does not work in real time (there is
+     *   no API for that), it only removes the "remember me" cookie
+     * - This is also true if you invalidate your current session: you will still be able to use it until you close
+     *   your browser or that the session times out. But automatic login (the "remember me" cookie) will not work
+     *   anymore.
+     *   There is an API to invalidate the current session, but there is no API to check which session uses which
+     *   cookie.
+     *
+     * @param series the series of an existing session
+     * @throws UnsupportedEncodingException if the series couldnt be URL decoded
+     */
+    @DeleteMapping("/account/sessions/{series}")
+    @Timed
+    @Throws(UnsupportedEncodingException::class)
+    fun invalidateSession(@PathVariable series: String) {
+        val decodedSeries = URLDecoder.decode(series, "UTF-8")
+        SecurityUtils.getCurrentUserLogin()
+            .flatMap<User> { this.userRepository.findOneByLogin(it) }
+            .ifPresent {
+                persistentTokenRepository.findByUser(it).stream()
+                    .filter { persistentToken -> StringUtils.equals(persistentToken.series, decodedSeries) }<% if (databaseType === 'sql' || databaseType === 'mongodb') { %>
+                    .findAny().ifPresent { t -> persistentTokenRepository.deleteById(decodedSeries) }<% } else if (databaseType === 'couchbase'){ %>
+                    .findAny().ifPresent { t -> persistentTokenRepository.deleteBySeries(decodedSeries)}<% } else { %>
+                    .findAny().ifPresent { persistentTokenRepository.delete(it) }<% } %>
+            }
+    }<% } %>
+
+    /**
+     * POST   /account/reset-password/init : Send an email to reset the password of the user
+     *
+     * @param mail the mail of the user
+     * @throws EmailNotFoundException 400 (Bad Request) if the email address is not registered
+     */
+    @PostMapping(path = arrayOf("/account/reset-password/init"))
+    @Timed
+    fun requestPasswordReset(@RequestBody mail: String) {
+        mailService.sendPasswordResetMail(
+            userService.requestPasswordReset(mail)
+                .orElseThrow<EmailNotFoundException>({ EmailNotFoundException() })
+        )
+    }
+
+    /**
+     * POST   /account/reset-password/finish : Finish to reset the password of the user
+     *
+     * @param keyAndPassword the generated key and the new password
+     * @throws InvalidPasswordException 400 (Bad Request) if the password is incorrect
+     * @throws RuntimeException 500 (Internal Server Error) if the password could not be reset
+     */
+    @PostMapping(path = arrayOf("/account/reset-password/finish"))
+    @Timed
+    fun finishPasswordReset(@RequestBody keyAndPassword: KeyAndPasswordVM) {
+        if (!checkPasswordLength(keyAndPassword.newPassword!!)) {
+            throw InvalidPasswordException()
+        }
+        val user = userService.completePasswordReset(keyAndPassword.newPassword!!, keyAndPassword.key!!)
+
+        if (!user.isPresent) {
+            throw InternalServerErrorException("No user was found for this reset key")
+        }
+    }
+
+    private fun checkPasswordLength(password: String): Boolean {
+        return !StringUtils.isEmpty(password) &&
+            password.length >= ManagedUserVM.PASSWORD_MIN_LENGTH &&
+            password.length <= ManagedUserVM.PASSWORD_MAX_LENGTH
+    }
+}
+<%_ } _%>
