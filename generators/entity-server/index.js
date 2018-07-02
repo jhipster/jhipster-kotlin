54688b63382d3484c1a56d5690be1137ee3de868
Holger Berndt <hberndt@bidcore.de>
2018-07-02 23:04:49 +0200

    Issue #6 corrections

--- generators/entity-server/index.js
+++ generators/entity-server/index.js
@@ -19,27 +19,9 @@ module.exports = class extends EntityServerGenerator {
         }
     }
 
-    get initializing() {
-        return super._initializing();
-    }
-
-    get prompting() {
-        return super._prompting();
-    }
-
-    get configuring() {
-        return super._configuring();
-    }
-
-    get default() {
-        return super._default();
-    }
-
     get writing() {
         return writeFiles();
     }
 
-    get end() {
-        return super._end();
-    }
+    
 };
