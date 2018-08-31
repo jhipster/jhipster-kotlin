f809c1a06b9fbfc03af9c389742dda5d4c1cdb51
Thodoris Bais <thodoris.bais@gmail.com>
2018-08-31 12:25:39 +0200

    Removed unused import, used prompting from base

--- generators/spring-service/prompts.js
+++ generators/spring-service/prompts.js
@@ -16,83 +16,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const jhiCore = require('jhipster-core');
-
 module.exports = {
-    askForControllerActions
+    getPrompting
 };
 
-function askForControllerActions() {
-    const askForControllerAction = (done) => {
-        const prompts = [
-            {
-                type: 'confirm',
-                name: 'actionAdd',
-                message: 'Do you want to add an action to your controller?',
-                default: true
-            },
-            {
-                when: response => response.actionAdd === true,
-                type: 'input',
-                name: 'actionName',
-                validate: (input) => {
-                    if (!(/^([a-zA-Z0-9_]*)$/.test(input))) {
-                        return 'Your action name cannot contain special characters';
-                    } if (input === '') {
-                        return 'Your action name cannot be empty';
-                    } if (input.charAt(0) === input.charAt(0).toUpperCase()) {
-                        return 'Your action name cannot start with an upper case letter';
-                    } if (jhiCore.isReservedFieldName(input)) {
-                        return 'Your action name cannot contain a Java, Angular or React reserved keyword';
-                    }
-
-                    return true;
-                },
-                message: 'What is the name of your action?'
-            },
-            {
-                when: response => response.actionAdd === true,
-                type: 'list',
-                name: 'actionMethod',
-                message: 'What is the HTTP method of your action?',
-                choices: [
-                    {
-                        name: 'POST',
-                        value: 'Post'
-                    },
-                    {
-                        name: 'GET',
-                        value: 'Get'
-                    },
-                    {
-                        name: 'PUT',
-                        value: 'Put'
-                    },
-                    {
-                        name: 'DELETE',
-                        value: 'Delete'
-                    }
-                ],
-                default: 1
-            }
-        ];
-
-        this.prompt(prompts).then((props) => {
-            if (props.actionAdd) {
-                const controllerAction = {
-                    actionName: props.actionName,
-                    actionMethod: props.actionMethod
-                };
-
-                this.controllerActions.push(controllerAction);
-
-                askForControllerAction(done);
-            } else {
-                done();
-            }
-        });
-    };
-
-    const done = this.async();
-    askForControllerAction(done);
+function getPrompting() {
+    // Here we are not overriding this phase and hence its being handled by JHipster
+    return super._prompting();
 }
