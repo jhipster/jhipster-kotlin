f89d128efdd3405f1f28b1899ae0856106eac270
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-09-05 08:17:15 +0200

    Travis: use NPM instead of Yarn

--- travis/scripts/00-install-jhipster.sh
+++ travis/scripts/00-install-jhipster.sh
@@ -7,36 +7,7 @@ set -e
 ls -al "$HOME"
 
 #-------------------------------------------------------------------------------
-# Install JHipster Dependencies
-#-------------------------------------------------------------------------------
-cd "$HOME"
-if [[ "$TRAVIS_REPO_SLUG" == *"/jhipster-dependencies" ]]; then
-    echo "TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG"
-    echo "No need to clone jhipster-dependencies: use local version"
-
-    cd "$TRAVIS_BUILD_DIR"
-    ./mvnw clean install -Dgpg.skip=true
-
-elif [[ "$JHIPSTER_DEPENDENCIES_BRANCH" == "release" ]]; then
-    echo "No need to clone jhipster-dependencies: use release version"
-
-else
-    git clone "$JHIPSTER_DEPENDENCIES_REPO" jhipster-dependencies
-    cd jhipster-dependencies
-    if [ "$JHIPSTER_DEPENDENCIES_BRANCH" == "latest" ]; then
-        LATEST=$(git describe --abbrev=0)
-        git checkout -b "$LATEST" "$LATEST"
-    elif [ "$JHIPSTER_DEPENDENCIES_BRANCH" != "master" ]; then
-        git checkout -b "$JHIPSTER_DEPENDENCIES_BRANCH" origin/"$JHIPSTER_DEPENDENCIES_BRANCH"
-    fi
-    git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-
-    ./mvnw clean install -Dgpg.skip=true
-    ls -al ~/.m2/repository/io/github/jhipster/jhipster-dependencies/
-fi
-
-#-------------------------------------------------------------------------------
-# Install JHipster lib
+# Install JHipster Dependencies and Server-side library
 #-------------------------------------------------------------------------------
 cd "$HOME"
 if [[ "$TRAVIS_REPO_SLUG" == *"/jhipster" ]]; then
@@ -60,8 +31,12 @@ else
     fi
     git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
+    travis/scripts/00-replace-version-jhipster.sh
+
     ./mvnw clean install -Dgpg.skip=true
-    ls -al ~/.m2/repository/io/github/jhipster/jhipster/
+    ls -al ~/.m2/repository/io/github/jhipster/jhipster-framework/
+    ls -al ~/.m2/repository/io/github/jhipster/jhipster-dependencies/
+    ls -al ~/.m2/repository/io/github/jhipster/jhipster-parent/
 fi
 
 #-------------------------------------------------------------------------------
@@ -73,14 +48,14 @@ if [[ "$TRAVIS_REPO_SLUG" == *"/generator-jhipster" ]]; then
     echo "No need to clone generator-jhipster: use local version"
 
     cd "$TRAVIS_BUILD_DIR"/
-    yarn install
-    yarn global add file:"$TRAVIS_BUILD_DIR"
+    npm install
+    npm install -g "$TRAVIS_BUILD_DIR"
     if [[ "$JHIPSTER" == "" || "$JHIPSTER" == "ngx-default" ]]; then
-        yarn test
+        npm test
     fi
 
 elif [[ "$JHIPSTER_BRANCH" == "release" ]]; then
-    yarn global add generator-jhipster
+    npm install -g generator-jhipster
 
 else
     git clone "$JHIPSTER_REPO" generator-jhipster
@@ -93,9 +68,8 @@ else
     fi
     git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
-    yarn install
-    yarn global add file:"$HOME"/generator-jhipster
-    yarn link
+    npm install
+    npm install -g "$HOME"/generator-jhipster
 fi
 
 #-------------------------------------------------------------------------------
@@ -107,8 +81,8 @@ ls -al "$HOME"
 # Install JHipster Kotlin
 #-------------------------------------------------------------------------------
 cd "$TRAVIS_BUILD_DIR"/
-yarn link generator-jhipster
-yarn install
+npm link generator-jhipster
+npm install
 # TODO: need to be removed as soon as there are some tests
-# yarn test
-yarn link
+# npm test
+npm link
