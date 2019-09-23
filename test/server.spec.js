efdf40a4e37bed3892b20e04691b8ce7a38683c6
Sendil Kumar Nellaiyapen <sendilkumarn@live.com>
2019-09-23 20:25:24 +0200

    upgrade version in package json
    test: update to version 6.3.0
    
    Update JHipster version
    
    Update JHipster version
    
    fix: update tests
    
    fix tests
    
    fix: add AuditEventServiceIT
    
    fix: files file
    
    Rename AuditEventServiceIt.kt.ejs to AuditEventServiceIT.kt.ejs
    
    fix: entity query Service
    
    Update EntityResourceIT.kt.ejs
    
    Update EntityResourceIT.kt.ejs
    
    Update AuditEventServiceIT.kt.ejs
    
    Update ExceptionTranslatorTestController.kt.ejs
    
    Regex should be explicit in Kotlin
    
    fix: id should not be optional
    
    fix: cache configuration
    
    fix: cache configuration
    
    feat: update to JHipster v6.3.1
    
    update package-lock.json
    
    fix issues

--- test/server.spec.js
+++ test/server.spec.js
@@ -6,7 +6,7 @@ const getFilesForOptions = require('./utils/utils').getFilesForOptions;
 const expectedFiles = require('./utils/expected-files');
 
 describe('JHipster server generator', () => {
-    describe('generate server', () => {
+    describe('generate server with ehcache', () => {
         before(done => {
             helpers
                 .run('generator-jhipster/generators/server')
@@ -46,6 +46,69 @@ describe('JHipster server generator', () => {
         });
 
         it('creates expected files for default configuration for server generator', () => {
+            assert.noFile(expectedFiles.common);
+            assert.file(expectedFiles.server);
+            assert.file(expectedFiles.jwtServer);
+            assert.file(expectedFiles.userManagementServer);
+            assert.file(expectedFiles.maven);
+            assert.file(expectedFiles.mysql);
+            assert.file(expectedFiles.hibernateTimeZoneConfig);
+            assert.noFile(
+                getFilesForOptions(
+                    angularfiles,
+                    {
+                        enableTranslation: true,
+                        serviceDiscoveryType: false,
+                        authenticationType: 'jwt',
+                        testFrameworks: []
+                    },
+                    null,
+                    ['package.json']
+                )
+            );
+        });
+    });
+
+    describe('generate server with caffeine', () => {
+        before(done => {
+            helpers
+                .run('generator-jhipster/generators/server')
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprint: 'kotlin',
+                    skipChecks: true,
+                    'skip-ktlint-format': true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/server/index.js'), // eslint-disable-line global-require
+                        'jhipster-kotlin:server',
+                        path.join(__dirname, '../generators/server/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    baseName: 'jhipster',
+                    packageName: 'com.mycompany.myapp',
+                    packageFolder: 'com/mycompany/myapp',
+                    serviceDiscoveryType: false,
+                    authenticationType: 'jwt',
+                    cacheProvider: 'caffeine',
+                    enableHibernateCache: true,
+                    databaseType: 'sql',
+                    devDatabaseType: 'h2Memory',
+                    prodDatabaseType: 'mysql',
+                    enableTranslation: true,
+                    nativeLanguage: 'en',
+                    languages: ['fr'],
+                    buildTool: 'maven',
+                    rememberMeKey: '5c37379956bd1242f5636c8cb322c2966ad81277',
+                    serverSideOptions: []
+                })
+                .on('end', done);
+        });
+
+        it('creates expected files for caffeine cache configuration for server generator', () => {
             assert.noFile(expectedFiles.common);
             assert.file(expectedFiles.server);
             assert.file(expectedFiles.jwtServer);
