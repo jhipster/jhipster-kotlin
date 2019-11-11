7643290aed90ce2c639864b220320aff81dc71b3
Vishal Mahajan <vishal423@gmail.com>
2019-11-11 13:03:56 +0530

    feat: Integrate github CI and move few build pipelines from Travis (#205)

--- test-integration/scripts/00-init-env.sh
+++ test-integration/scripts/00-init-env.sh
@@ -6,15 +6,17 @@ init_var() {
         result=$1
     elif [[ $2 != "" ]]; then
         result=$2
+    elif [[ $3 != "" ]]; then
+        result=$3
     fi
     echo $result
 }
 
 # uri of repo
-JHI_REPO=$(init_var "$BUILD_REPOSITORY_URI" "$TRAVIS_REPO_SLUG")
+JHI_REPO=$(init_var "$BUILD_REPOSITORY_URI" "$TRAVIS_REPO_SLUG" "$GITHUB_WORKSPACE" )
 
 # folder where the repo is cloned
-JHI_CLONED=$(init_var "$BUILD_REPOSITORY_LOCALPATH" "$TRAVIS_BUILD_DIR")
+JHI_CLONED=$(init_var "$BUILD_REPOSITORY_LOCALPATH" "$TRAVIS_BUILD_DIR" "$GITHUB_WORKSPACE")
 
 # folder where the generator-jhipster is cloned
 JHI_HOME="$HOME"/generator-jhipster
@@ -35,6 +37,6 @@ JHI_FOLDER_APP="$HOME"/app
 JHI_FOLDER_UAA="$HOME"/uaa
 
 # set correct OpenJDK version
-if [[ "$JHI_JDK" == "11" ]]; then
+if [[ "$JHI_JDK" == "11" && "$JHI_GITHUB_CI" != "true" ]]; then
     JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 fi
