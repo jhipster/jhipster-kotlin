79ae3d4331157d16e566042ae5a9f7c3d0a4b281
sendilkumarn <sendilkumarn@live.com>
2021-11-23 22:49:02 +0100

    fix: upgrade mapstruct to v1.4.2.final

--- generators/generator-kotlin-constants.js
+++ generators/generator-kotlin-constants.js
@@ -23,7 +23,7 @@ const KTLINT_MAVEN_VERSION = '1.7.0';
 const KTLINT_GRADLE_VERSION = '10.1.0';
 const DETEKT_VERSION = '1.1.0';
 const MAVEN_ANTRUN_VERSION = '3.0.0';
-const MAPSTRUCT_VERSION = '1.4.1.Final';
+const MAPSTRUCT_VERSION = '1.4.2.Final';
 
 const DETEKT_CONFIG_FILE = 'detekt-config.yml';
 
