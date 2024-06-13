a9bebef73f360ae737fb834318bf11efd546b3ad
Marcelo Boveto Shima <marceloshima@gmail.com>
2024-06-13 17:08:29 -0300

    update cli

--- cli/khipster.js
+++ cli/khipster.js
@@ -17,76 +17,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const semver = require('semver');
-const path = require('path');
-const { logger } = require('generator-jhipster/cli/utils');
-const packageJson = require('../package.json');
 
-const currentNodeVersion = process.versions.node;
-const minimumNodeVersion = packageJson.engines.node;
+const { dirname, basename } = require('path');
+const { version, bin } = require('../package.json');
 
-if (!semver.satisfies(currentNodeVersion, minimumNodeVersion)) {
-    /* eslint-disable no-console */
-    logger.error(
-        `You are running Node version ${currentNodeVersion}\nKHipster requires Node version ${minimumNodeVersion}\nPlease update your version of Node.`
-    );
-    /* eslint-enable  */
-}
+// Get package name to use as namespace.
+// Allows blueprints to be aliased.
+const packagePath = dirname(__dirname);
+const packageFolderName = basename(packagePath);
+const blueprint = packageFolderName.startsWith('jhipster-') ? `generator-${packageFolderName}` : packageFolderName;
 
-let preferLocal = true;
+(async () => {
+  const { runJHipster, done, logger } = await import('generator-jhipster/esm/cli');
+  const executableName = Object.keys(bin)[0];
 
-// Don't use commander for parsing command line to avoid polluting it in cli.js
-// --prefer-local: Always resolve node modules locally (useful when using linked module)
-if ((process.argv.includes('upgrade') && !process.argv.includes('--prefer-local')) || process.argv.includes('--prefer-global')) {
-    // Prefer global version for `jhipster upgrade` to get most recent code
-    preferLocal = false;
-}
+  runJHipster({
+    executableName,
+    executableVersion: version,
+    defaultCommand: 'app',
+    blueprints: {
+      [blueprint]: version,
+    },
+    printLogo: () => {},
+    lookups: [{ packagePaths: [packagePath], lookups: ['generators'] }],
+  }).catch(done);
 
-// Pass in kotlin as a blueprint module.
-// User passes in blueprints flag but without Kotlin :> append Kotlin
-if (!process.argv.includes('kotlin') && process.argv.includes('--blueprints')) {
-    for (let i = 0; i < process.argv.length; i++) {
-        if (process.argv[i] === '--blueprints') {
-            process.argv[i + 1] = `${process.argv[i + 1].split(',')},kotlin`;
-        }
-    }
-    // User passes in blueprint flag but without Kotlin :> append Kotlin
-} else if (!process.argv.includes('kotlin') && process.argv.includes('--blueprint')) {
-    for (let i = 0; i < process.argv.length; i++) {
-        if (process.argv[i] === '--blueprint') {
-            process.argv[i] = '--blueprints';
-            process.argv[i + 1] = `${process.argv[i + 1]},kotlin`;
-        }
-    }
-    // User donot pass in blueprints or blueprint flag but without Kotlin :> append Kotlin
-} else if (!process.argv.includes('kotlin') && !process.argv.includes('--blueprint') && !process.argv.includes('--blueprints')) {
-    process.argv.push('--blueprints');
-    process.argv.push('kotlin');
-}
-
-requireCLI(preferLocal);
-
-/*
- * Require cli.js giving priority to local version over global one if it exists.
- */
-function requireCLI(preferLocal) {
-    /* eslint-disable global-require */
-    if (preferLocal) {
-        try {
-            const localCLI = require.resolve(path.join(process.cwd(), 'node_modules', 'generator-jhipster', 'cli', 'cli.js'));
-            if (__dirname !== path.dirname(localCLI)) {
-                // load local version
-                /* eslint-disable import/no-dynamic-require */
-                logger.info("Using KHipster version installed locally in current project's node_modules");
-                require(localCLI);
-                return;
-            }
-        } catch (e) {
-            // Unable to find local version, so global one will be loaded anyway
-        }
-    }
-    // load global version
-    logger.info('Using JHipster version installed globally');
-    require('generator-jhipster/cli/cli');
-    /* eslint-enable  */
-}
+  process.on('unhandledRejection', up => {
+    logger.error('Unhandled promise rejection at:');
+    logger.fatal(up);
+  });
+})();
