21f898b2fdcb1752fc8447016a4f77b3414961c8
Marcelo Shima <marceloshima@gmail.com>
2025-01-28 18:16:13 -0300

    adjusts

--- .blueprint/github-build-matrix/generator.spec.mjs
+++ .blueprint/github-build-matrix/generator.spec.mjs
@@ -1,7 +1,7 @@
 import { basename, dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { beforeAll, describe, expect, it } from 'vitest';
-import { defaultHelpers as helpers, runResult } from 'generator-jhipster/testing';
+import { getGithubSamplesGroups, defaultHelpers as helpers, runResult } from 'generator-jhipster/testing';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,8 @@ const __dirname = dirname(__filename);
 const generator = basename(__dirname);
 
 describe(`generator - ${generator}`, async () => {
-    const groups = ['angular', 'react'];
-    for (const workflow of groups.map(sample => sample.split('.')[0])) {
+    const groups = await getGithubSamplesGroups(join(__dirname, '../generate-sample/templates/'));
+    for (const workflow of groups.filter(group => !group.startsWith('_')).map(sample => sample.split('.')[0])) {
         describe(`with ${workflow}`, () => {
             beforeAll(async () => {
                 await helpers.runJHipster(join(__dirname, 'index.mjs'), { useEnvironmentBuilder: true }).withArguments(workflow);
