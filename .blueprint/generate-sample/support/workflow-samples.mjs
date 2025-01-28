4af3c249a7ce104317d515ec1d1056d136793caa
Marcelo Shima <marceloshima@gmail.com>
2025-01-28 18:09:24 -0300

    cleanup test-integration and github actions

--- .blueprint/generate-sample/support/workflow-samples.mjs
+++ .blueprint/generate-sample/support/workflow-samples.mjs
@@ -1,9 +1,9 @@
 import { readFileSync } from 'fs';
 
-const angularJson = readFileSync(new URL('../../../test-integration/workflow-samples/angular.json', import.meta.url), 'utf8');
+const angularJson = readFileSync(new URL('../templates/workflow-samples/angular.json', import.meta.url), 'utf8');
 const angularWorkflowSamples = JSON.parse(angularJson).include;
 
-const reactJson = readFileSync(new URL('../../../test-integration/workflow-samples/react.json', import.meta.url), 'utf8');
+const reactJson = readFileSync(new URL('../templates/workflow-samples/react.json', import.meta.url), 'utf8');
 const reactWorkflowSamples = JSON.parse(reactJson).include;
 
 const prepareSamples = samples => Object.fromEntries(samples.map(({ name, ...sample }) => [name, sample]));
--- test-integration/scripts/14-jhipster-info.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-source $(dirname $0)/00-init-env.sh
-
-cd "$KHI_FOLDER_APP"
-cli.cjs info
